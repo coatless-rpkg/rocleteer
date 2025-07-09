dbeb34df5d8705d08fa9c6fa85622901b7b5a72b
james.balamuta@gmail.com <james.balamuta@gmail.com>
2025-07-09 01:41:00 -0700

    Add template insert

--- R/rstudio-addins.R
+++ R/rstudio-addins.R
@@ -17,3 +17,24 @@ insert_examplesTempdir_template <- function() {
   
   rstudioapi::insertText(template)
 }
+
+#' Insert a webR examples template
+#'
+#' This function inserts a template for the `@examplesWebR` tag at the cursor position.
+#' It's designed to be used as an RStudio addin.
+#'
+#' @export
+insert_examplesWebR_template <- function() {
+  if (!requireNamespace("rstudioapi", quietly = TRUE) || !rstudioapi::isAvailable()) {
+    stop("This function must be used within RStudio")
+  }
+  
+  template <- paste(
+    "#' @examplesWebR",
+    "#' # Your interactive R code here",
+    "#' plot(1:10, 1:10)",
+    sep = "\n"
+  )
+  
+  rstudioapi::insertText(template)
+}
